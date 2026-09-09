b238196aea7eca85cf6931a79c2ff1934316780a
Sujju-192 <2512surajdas@gmail.com>
2026-09-09 22:14:57 +0530

    The youtube chatbot is working properly only the pdf one has issue loading the pdf in vectorstore

--- backend/main.py
+++ backend/main.py
@@ -33,7 +33,7 @@ load_dotenv()
 embedding_model = GoogleGenerativeAIEmbeddings(model="models/gemini-embedding-001")
 llm_model = ChatGoogleGenerativeAI(model="gemini-2.5-flash")
 
-parser = StrOutputParser
+parser = StrOutputParser()
 
 app = FastAPI(title="OmniRAG Gemini API")
 
